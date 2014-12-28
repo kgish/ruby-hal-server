09a92ee88ec24c578365329cb0d915ce8bba37e2
Kiffin Gish <kiffin.gish@planet.nl>
2014-12-28 18:57:13 +0100

    Loops forever until sigint

--- client-show.rb
+++ client-show.rb
@@ -2,23 +2,32 @@ require 'bundler/setup'
 require 'httparty'
 require 'json'
 
-puts 'GET /products ALL'
-response = HTTParty.get('http://localhost:8080/products', :headers => {'Content-type' => 'application/json'})
-puts response.code, response.headers.inspect, response.message
+trap("SIGINT") { throw :ctrl_c }
 
-h = JSON.parse response.body
-#puts h['products']
+catch :ctrl_c do
+  loop do
+    system('clear')
+    puts 'GET /products ALL'
+    response = HTTParty.get('http://localhost:8080/products', :headers => {'Content-type' => 'application/json'})
+    puts response.code, response.headers.inspect, response.message
 
-cnt = 0
-h['products'].each do |key|
-if cnt == 0
-  puts "#\tid\tname\tprice\tcategory"
-  puts "-\t--\t----\t-----\t--------"
+    h = JSON.parse response.body
+
+    cnt = 0
+    h['products'].each do |key|
+    if cnt == 0
+      puts "#\tid\tname\tprice\tcategory"
+      puts "-\t--\t----\t-----\t--------"
+    end
+      p = key['product']
+      cnt = cnt + 1
+      puts "#{cnt}\t#{p['id']}\t#{p['name']}\t#{p['price']}\t#{p['category']}"
+    end
+
+    puts ' '
+    puts "There are -#{cnt}- products"
+    puts ' '
+    puts 'CTRL-C to exit'
+    sleep(1)
+  end
 end
-  p = key['product']
-  cnt = cnt + 1
-  puts "#{cnt}\t#{p['id']}\t#{p['name']}\t#{p['price']}\t#{p['category']}"
-end
-
-puts ' '
-puts "There are -#{cnt}- products"
