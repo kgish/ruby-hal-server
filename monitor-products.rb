d61b02a1b2475107ee89ecb6136aeabe47049007
Kiffin Gish <kiffin.gish@planet.nl>
2014-12-31 09:42:32 +0100

    getopts including help screen using --help

--- monitor-products.rb
+++ monitor-products.rb
@@ -2,8 +2,10 @@ require 'bundler/setup'
 require 'httparty'
 require 'json'
 
-host = '0.0.0.0'
-port = 8080
+params = get_params(false)
+
+host = params[:host]
+port = params[:port]
 resource = 'products'
 url = "http://#{host}:#{port}/#{resource}"
 
@@ -54,3 +56,118 @@ catch :ctrl_c do
     sleep(1)
   end
 end
+
+BEGIN {
+  require 'getoptlong'
+
+  def show_usage(message, defaults)
+    puts "monitor-product.rb: #{message}" if message
+    puts <<-EOF
+
+  USAGE:
+
+    monitor-products [OPTIONS]
+
+  DESCRIPTION:
+
+    Monitors the list or products for the given server by looping through
+    the following HTTP request:
+
+      GET /products
+
+    Once started hit CTRL-C to exit from the loop.
+
+  OPTIONAL PARAMETERS:
+
+    --help, -h
+       show this help screen
+
+    --auth, -a username:password
+       basic authorization string (both username and password required)
+
+    --url, -u hostname[:port]
+       destination of request (default #{defaults[:host]}:#{defaults[:port]})
+
+  EXAMPLES:
+
+    monitor-products
+    monitor-products --url=localhost:8080
+    monitor-products --auth=kiffin:pindakaas
+
+    EOF
+    exit 0
+  end
+
+  def show_params(params)
+    if params[:auth]
+      authorization = "auth:'#{params[:auth]}'"
+    else
+      authorization = '(no authorization)'
+    end
+    puts "url:'#{params[:url]}' name:'#{params[:id]}' #{authorization}"
+  end
+
+  def get_params(show)
+    defaults = {
+        host: '0.0.0.0',
+        port: 8080
+    }
+
+    params = {
+        url: nil,
+        host: defaults[:host],
+        port: defaults[:port],
+        username: nil,
+        password: nil,
+        auth: nil,
+    }
+
+    opts = GetoptLong.new(
+        [ '--help',     '-h', GetoptLong::NO_ARGUMENT       ],
+        [ '--auth',     '-a', GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--url',      '-u', GetoptLong::REQUIRED_ARGUMENT ]
+    )
+
+    begin
+      opts.each do |opt, arg|
+        case opt
+          when '--help'
+            show_usage(nil, defaults)
+          when '--auth'
+            (username,password) = arg.split(':')
+            unless password
+              show_usage("password required for username -- '#{username}'", defaults)
+            end
+            params[:username] = username
+            params[:password] = password
+          when '--url'
+            (host,port) = arg.split(':')
+            if port
+              unless /^\d+$/ === port
+                show_usage("invalid port -- #{port} (must be a number)", defaults)
+              end
+            end
+            params[:host] = host
+            params[:port] = port
+        end
+      end
+    rescue GetoptLong::Error => e
+      show_usage(nil, defaults)
+    end
+
+    # Check url = "hostname:port"
+    params[:host] ||= defaults[:host]
+    params[:port] ||= defaults[:port]
+    params[:url] = "#{params[:host]}:#{params[:port]}"
+
+    # Check authorization = "username:password"
+    params[:auth] = "#{params[:username]}:#{params[:password]}" if params[:username] && params[:password]
+
+    # Check no extra arguments
+    show_usage("no extra arguments allowed -- '#{ARGV}'", defaults) unless ARGV.length == 0
+
+    show_params(params) if show
+
+    return params
+  end
+}
