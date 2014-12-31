31ce8cf679e53f5f7a40f00a08ee651c458eec38
Kiffin Gish <kiffin.gish@planet.nl>
2014-12-31 16:05:38 +0100

    First attempt at cookie authorization using the is_authorized? method.

--- resources/resource.rb
+++ resources/resource.rb
@@ -7,35 +7,16 @@ class Resource < Webmachine::Resource
   end
 
   let(:trace?) { true }
-  let(:content_types_provided) { [["application/json", :to_json]] }
-  let(:content_types_accepted) { [["application/json", :from_json]] }
+  let(:content_types_provided) { [['application/json', :to_json]] }
+  let(:content_types_accepted) { [['application/json', :from_json]] }
   let(:post_is_create?) { true }
   let(:allow_missing_post?) { true }
   let(:from_json) { JSON.parse(request.body.to_s)['data'] }
 
-  #let(:create_path) { "/evaluations/#{create_resource.id}" }
-  #let(:from_json) { request.body.rewind; JSON.parse(request.body.read) }
-  #let(:to_json) { resource_or_collection.to_json }
+  def is_authorized?(auth)
+    puts "Resource: is_authorized? => cookies:#{request.cookies.inspect}"
+    true
+  end
 
-  # protected
-  #
-  #   def parsed_body
-  #     @parsed_body ||= JSON.parse(request.body.to_s)
-  #   end
 end
 
-#class ProductResource < Webmachine::Resource
-#
-#  def resource_exists?
-#    @product = $products[request.path_info[:id].to_i - 1] if request.path_info[:id]
-#    !@product.nil?
-#  end
-#
-#  def from_json
-#    $products << @product.from_json(request.body.to_s, :except => [:id])
-#  end
-#  
-#  def to_json
-#    @product.to_json
-#  end
-#end
